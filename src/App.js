ded30547ea4b2778d2ad87c4fa110120555cfc0b
Michelle Harley <chell.hx@gmail.com>
2023-07-07 20:38:53 -0400

    basic form of data showing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      let result = await axios.get("http://localhost:3001/summary");
+      setTransactions(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return (
+    <div className="App">
+      {transactions.map((transaction) => {
+        return (
+          <div key={transaction.id}>
+            {transaction.date} / {transaction.category} / {transaction.amount}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
