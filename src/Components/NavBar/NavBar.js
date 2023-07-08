06b18e398658961abc51ffa51a4b7ae7294d7416
Michelle Harley <chell.hx@gmail.com>
2023-07-07 23:04:11 -0400

    nav css done

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./NavBar.css";
+
 function NavBar() {
   return (
-    <nav>
-      <Link to="/">BagCheck</Link>
-      <button>New Transaction </button>
+    <nav className="navbar">
+      <Link to="/">
+        <span className="title">BagCheck</span>
+      </Link>
+
+      <button className="button">
+        <Link to="create-new-transaction">New Transaction</Link>
+      </button>
     </nav>
   );
 }
