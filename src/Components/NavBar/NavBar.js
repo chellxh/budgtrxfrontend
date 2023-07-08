d65678ec03fbfdfc3de757bba5ad61ab6588a5ac
Michelle Harley <chell.hx@gmail.com>
2023-07-07 21:31:28 -0400

    navBar component skeleton

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <Link to="/">BagCheck</Link>
+      <button>New Transaction </button>
+    </nav>
+  );
 }
 
 export default NavBar;
