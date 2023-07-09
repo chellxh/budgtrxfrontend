4b3ea9e03861269237dbf1a0aae1c5cde6bb5774
Michelle Harley <chell.hx@gmail.com>
2023-07-08 22:15:21 -0400

    transaction & home components made and styled

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>
+        Check The Bag with <br />
+        <span>BagCheck</span>
+      </h1>
+      <div className="transactionsLink"><Link to="/transactions">View Transactions Here</Link></div>
+    </div>
+  );
 }
 
 export default Home;
--- src/Components/Transactions/Transactions.js
+++ src/Components/Transactions/Transactions.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Transactions.css";
 
 function Transactions() {
   const [transactions, setTransactions] = useState([]);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     fetchData();
@@ -12,20 +15,52 @@ function Transactions() {
     try {
       let result = await axios.get("http://localhost:3001/summary");
       setTransactions(result.data);
+      getBalance();
     } catch (e) {
       console.log(e);
     }
   }
 
+  function getBalance() {
+    let newBalance = 0;
+    transactions.forEach(({ id, amount }) => {
+      if (id) {
+        newBalance += amount;
+      }
+    });
+    setBalance(newBalance);
+  }
+
   return (
-    <div className="transactions">
-      {transactions.map((transaction) => {
-        return (
-          <div key={transaction.id}>
-            {transaction.date} / {transaction.from} / {transaction.amount}
-          </div>
-        );
-      })}
+    <div>
+      <h2 className="trans-h2">Transactions</h2>
+      <h3 className="balance">Current Balance - {balance}</h3>
+      <div className="transactions">
+        <table id="transactions-table">
+          <tbody>
+            <tr>
+              <th>Date</th>
+              <th>From</th>
+              <th>Amount</th>
+            </tr>
+            {transactions.map(({ id, date, from, amount }) => {
+              return (
+                <tr id="trans-row" key={id}>
+                  <td>
+                    <Link to={`/transactions/${id}`}>{date}</Link>
+                  </td>
+                  <td>
+                    <Link to={`/transactions/${id}`}>{from}</Link>
+                  </td>
+                  <td>
+                    <Link to={`/transactions/${id}`}>{amount}</Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
